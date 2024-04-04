e99bd77e2e9abfeb5f57bae4e6ab708339ed9f00
Huy Nguyen <danghuy1999@gmail.com>
2024-04-04 22:14:15 +0700

    [c] Remove unused settings

--- headless_cms/settings.py
+++ headless_cms/settings.py
@@ -1,8 +1,7 @@
-from contextlib import contextmanager
 from typing import Any
 
 from django.conf import settings
-from rest_framework.settings import APISettings, perform_import
+from rest_framework.settings import APISettings
 
 HEADLESS_CMS_DEFAULTS: dict[str, Any] = {
     "AUTO_TRANSLATE_CLASS": "headless_cms.auto_translate.BaseTranslate",
@@ -19,35 +18,9 @@ IMPORT_STRINGS = [
 class HeadlessCMSSettings(APISettings):
     _original_settings: dict[str, Any] = {}
 
-    def apply_patches(self, patches):
-        for attr, val in patches.items():
-            if attr in self.import_strings:
-                val = perform_import(val, attr)  # noqa
-            # load and store original value, then override __dict__ entry
-            self._original_settings[attr] = getattr(self, attr)
-            setattr(self, attr, val)
-
-    def clear_patches(self):
-        for attr, orig_val in self._original_settings.items():
-            setattr(self, attr, orig_val)
-        self._original_settings = {}
-
 
 headless_cms_settings = HeadlessCMSSettings(
     user_settings=getattr(settings, "HEADLESS_CMS_SETTINGS", {}),  # type: ignore
     defaults=HEADLESS_CMS_DEFAULTS,  # type: ignore
     import_strings=IMPORT_STRINGS,
 )
-
-
-@contextmanager
-def patched_settings(patches):
-    """temporarily patch the global spectacular settings (or do nothing)"""
-    if not patches:
-        yield
-    else:
-        try:
-            headless_cms_settings.apply_patches(patches)
-            yield
-        finally:
-            headless_cms_settings.clear_patches()
